209dbc723bc2fecc7c75b2773fa1c6825518c501
Petra Školová <petraskolova13@gmail.com>
2023-11-15 11:06:33 +0100

    add 12_tkinter and 12-DU

--- 11-DU/uloha.py
+++ 11-DU/uloha.py
@@ -26,9 +26,7 @@ canvas.create_rectangle(x+d, y+3*d, x+2*d, y+4*d)
 canvas.create_rectangle(x+2*d, y+3*d, x+3*d, y+4*d)
 canvas.create_rectangle(x+3*d, y+3*d, x+4*d, y+4*d)
 # pismeno E
-x = 6 * d
-y = 10
-d = 20
+
 canvas.create_rectangle(x, y, x+d, y+d)
 canvas.create_rectangle(x, y+d, x+d, y+2*d)
 canvas.create_rectangle(x, y+2*d, x+d, y+3*d)
@@ -51,9 +49,7 @@ canvas.create_rectangle(x+2*d, y+6*d, x+3*d, y+7*d)
 canvas.create_rectangle(x+3*d, y+6*d, x+4*d, y+7*d)
 canvas.create_rectangle(x+4*d, y+6*d, x+5*d, y+7*d)
 # pismeno T
-x = 12 * d
-y = 10
-d = 20
+
 canvas.create_rectangle(x, y, x+d, y+d)
 canvas.create_rectangle(x+d, y, x+2*d, y+d)
 canvas.create_rectangle(x+2*d, y, x+3*d, y+d)
@@ -67,9 +63,7 @@ canvas.create_rectangle(x+2*d, y+4*d, x+3*d, y+5*d)
 canvas.create_rectangle(x+2*d, y+5*d, x+3*d, y+6*d)
 canvas.create_rectangle(x+2*d, y+6*d, x+3*d, y+7*d)
 # pismeno R
-x = 18 * d
-y = 10
-d = 20
+
 canvas.create_rectangle(x, y, x+d, y+d)
 canvas.create_rectangle(x, y+d, x+d, y+2*d)
 canvas.create_rectangle(x, y+2*d, x+d, y+3*d)
@@ -93,9 +87,7 @@ canvas.create_rectangle(x+2*d, y+4*d, x+3*d, y+5*d)
 canvas.create_rectangle(x+3*d, y+5*d, x+4*d, y+6*d)
 canvas.create_rectangle(x+4*d, y+6*d, x+5*d, y+7*d)
 # pismeno A
-x = 24 * d
-y = 10
-d = 20
+
 canvas.create_rectangle(x+d, y, x+2*d, y+d)
 canvas.create_rectangle(x+2*d, y, x+3*d, y+d)
 canvas.create_rectangle(x+3*d, y, x+4*d, y+d)
---
+++ 12-tkinter2/main.py
@@ -0,0 +1,34 @@
+import tkinter
+import random
+
+canvas = tkinter.Canvas(width = 600, height = 500)
+canvas.pack()
+"""""
+x = 3
+xx = x
+y = 10
+d = 30
+
+pocet = 598 // d    # // je celociselne delenie 7 // 3 = 2
+for j in range(498 // d):
+    for i in range(pocet):
+        color = random.choice(("green", "red", "blue", "orange", "purple"))
+        canvas.create_rectangle(x, y, x+d, y+d, fill = color)
+        canvas.create_line(x, y, x+d, y+d)
+        canvas.create_line(x, y+d, x+d, y)
+        x = x + d  #nakresli stvorcek vedla dalsieho stvorceka
+    y = y + d
+    x = xx
+"""
+
+
+x = 50
+xx = x
+y = 50
+d = 80
+stvrtina = d // 4
+canvas.create_rectangle(x, y, x+d, y+d)
+canvas.create_polygon(50, 50, 50, 80, 80, 50)
+
+
+canvas.mainloop()
