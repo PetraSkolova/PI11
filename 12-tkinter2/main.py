cd9947e325e29b58d48a65ce32774420cec4b824
PetraSkolova <petraskolova13@gmail.com>
2023-11-16 18:22:57 +0100

    urobenie 1 domceku a vytvorenie python file pre DU

--- 12-tkinter2/main.py
+++ 12-tkinter2/main.py
@@ -27,8 +27,16 @@ xx = x
 y = 50
 d = 80
 stvrtina = d // 4
-canvas.create_rectangle(x, y, x+d, y+d)
-canvas.create_polygon(50, 50, 50, 80, 80, 50)
+
+#canvas.create_rectangle(x,  y+d // 2, x+d, y+(d // 2)+d)
+#canvas.create_polygon(x + d // 2, y, x+d, x+d, y+(d // 2)+d, x + d // 2, y)
+canvas.create_rectangle(x,  y+d // 2, x+d, y+(d // 2)+d, fill="orange")
+canvas.create_polygon(x, y+d//2, x+2*stvrtina, y, x+d, y+d//2, outline="black", fill="red")
+canvas.create_rectangle(x+stvrtina, y+d // 2+stvrtina, x+3*stvrtina, y+d // 2+3*stvrtina, fill="light blue")
+#canvas.create_line(x+stvrtina, y+d // 2+2*stvrtina, x+3*stvrtina,y+d // 2+2*stvrtina)
+#canvas.create_line()
+canvas.create_line(x+stvrtina, y+d // 2+2*stvrtina, x+3*stvrtina,y+d // 2+2*stvrtina)
+canvas.create_line(x+2*stvrtina, y+d // 2+stvrtina, x+2*stvrtina, y+d // 2+3*stvrtina)
 
 
 canvas.mainloop()
