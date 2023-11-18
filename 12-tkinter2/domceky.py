40f2797880c15ceb171e7944645645a9ba710811
PetraSkolova <petraskolova13@gmail.com>
2023-11-18 17:10:46 +0100

    dorobenie domcekov

---
+++ 12-tkinter2/domceky.py
@@ -0,0 +1,30 @@
+import tkinter
+import random
+
+canvas = tkinter.Canvas(width = 600, height = 500)
+canvas.pack()
+
+x = 10
+xx = x
+y = 10
+d = 40
+stvrtina = d // 4
+
+#canvas.create_rectangle(x,  y+d // 2, x+d, y+(d // 2)+d)
+#canvas.create_polygon(x + d // 2, y, x+d, x+d, y+(d // 2)+d, x + d // 2, y)
+pocet = 598 // d    # // je celociselne delenie 7 // 3 = 2
+for j in range(498 // d):
+    for i in range(pocet):
+        canvas.create_rectangle(x,  y+d // 2, x+d, y+(d // 2)+d, fill="orange")
+        canvas.create_polygon(x, y+d//2, x+2*stvrtina, y, x+d, y+d//2, outline="black", fill="red")
+        canvas.create_rectangle(x+stvrtina, y+d // 2+stvrtina, x+3*stvrtina, y+d // 2+3*stvrtina, fill="light blue")
+        #canvas.create_line(x+stvrtina, y+d // 2+2*stvrtina, x+3*stvrtina,y+d // 2+2*stvrtina)
+        canvas.create_line(x+stvrtina, y+d // 2+2*stvrtina, x+3*stvrtina,y+d // 2+2*stvrtina)
+        canvas.create_line(x+2*stvrtina, y+d // 2+stvrtina, x+2*stvrtina, y+d // 2+3*stvrtina)
+        x = x + d
+    y = y+3*d//2
+    x = xx
+
+
+
+canvas.mainloop()
--- 12-tkinter2/main.py
+++ 12-tkinter2/main.py
@@ -3,7 +3,7 @@ import random
 
 canvas = tkinter.Canvas(width = 600, height = 500)
 canvas.pack()
-"""""
+
 x = 3
 xx = x
 y = 10
@@ -19,23 +19,5 @@ for j in range(498 // d):
         x = x + d  #nakresli stvorcek vedla dalsieho stvorceka
     y = y + d
     x = xx
-"""
-
-
-x = 50
-xx = x
-y = 50
-d = 80
-stvrtina = d // 4
-
-#canvas.create_rectangle(x,  y+d // 2, x+d, y+(d // 2)+d)
-#canvas.create_polygon(x + d // 2, y, x+d, x+d, y+(d // 2)+d, x + d // 2, y)
-canvas.create_rectangle(x,  y+d // 2, x+d, y+(d // 2)+d)
-canvas.create_polygon(x, y+d//2, x+2*stvrtina, y, x+d, y+d//2, outline="black", fill="#f0f0ed")
-canvas.create_rectangle(x+stvrtina, y+d // 2+stvrtina, x+3*stvrtina, y+d // 2+3*stvrtina)
-#canvas.create_line(x+stvrtina, y+d // 2+2*stvrtina, x+3*stvrtina,y+d // 2+2*stvrtina)
-#canvas.create_line()
-canvas.create_line(x+stvrtina, y+d // 2+2*stvrtina, x+3*stvrtina,y+d // 2+2*stvrtina)
-canvas.create_line(x+2*stvrtina, y+d // 2+stvrtina, x+2*stvrtina, y+d // 2+3*stvrtina)
 
 canvas.mainloop()
